8da9c2ed9e3b7d0d5ca18563bccf8f1a8b9cb3e7
Nomoo0603 <60531777+Nomoo0603@users.noreply.github.com>
2022-10-08 14:43:44 +0800

    dd

---
+++ README.md
@@ -0,0 +1,13 @@
+# How Many Days Until Halloween
+
+A vanilla Javascript countdown to the spookiest day of the year.
+
+It can be seen online at [halloweenti.me](https://halloweenti.me) *if you dare!*
+
+![](assets/social-preview.png)
+
+## Opting Out of Hacktoberfest
+
+I made the decision not to opt this project into Hacktoberfest for 2020.
+
+The reasons why can be found in my blog post *[Opting Out Of Hacktoberfest](https://shannoncrabill.com/blog/opting-out-of-hacktoberfest/)*.
---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+    const date = document.querySelector("#date")
+    const title = document.querySelector("title")
+    const h1 = document.querySelector("h1")
+
+
+    const now = new Date()
+    let year = now.getFullYear()
+
+    if (now.getMonth() > 9) {
+        year += 1
+    }
+
+    const halloween = new Date(`October 31, ${year} 00:00:00`)
+    const timeUntil = halloween.getTime() - now.getTime()
+    const daysUntil = Math.abs(Math.ceil(timeUntil / (1000 * 60 * 60 * 24)))
+
+    switch (daysUntil) {
+        case 1:
+            h1.innerHTML = `Halloween is <span id="date">${daysUntil}</span> day away!`
+            title.innerHTML = `${daysUntil} Day Until Halloween!`
+            break;
+        case 0:
+            h1.innerHTML = `<span id="date">Today is Halloween!</span> Eat, drink and be scary!`
+            title.innerHTML = "Ahhhh! Today is Halloween!"
+            break;
+        default:
+            date.innerHTML = daysUntil
+            title.innerHTML = `${daysUntil} Days Until Halloween!`
+            break;
+    }
+
+});
